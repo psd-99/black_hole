29c406b68ef21c23702a0db17b4b711e2c37cf64
psd-99 <pasinduekanayaka99@gmail.com>
2024-07-11 23:34:07 +0530

    setup background

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
+//main
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "space app",
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(),
+      ),
+    );
+  }
 }
