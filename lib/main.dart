4da94e7c6d165b758f75b6108aa3ec9ee561ea41
psd-99 <pasinduekanayaka99@gmail.com>
2024-07-17 23:00:40 +0530

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "space app",
       home: Scaffold(
         backgroundColor: Colors.black,
